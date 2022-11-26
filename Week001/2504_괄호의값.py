2b7f51f6df97d53a7f39dcb90adce4b62a4c6e29
ChaeLinYeo <evergreen786@naver.com>
2022-11-26 21:43:40 +0900

    주석 추가

--- Week001/2504_괄호의값.py
+++ Week001/2504_괄호의값.py
@@ -3,6 +3,12 @@
 import sys
 input = sys.stdin.readline
 
+"""
+1. 열기/닫기 형식이 이전거랑 같으면 곱하기
+2. 열기/닫기 쌍이 맞으면 나누기
+3. 열기/닫기 쌍이 안 맞으면 더하기
+"""
+
 string = list(input()[:-1])
 stack = []
 answer = 0
@@ -14,12 +20,12 @@ for i in range(len(string)):
         stack.append(string[i])
         if i==0 or string[i-1] == "(" or string[i-1] == "[" \
         or string[i-1] == ")" or string[i-1] == "]":
-            tmp*=2
+            tmp*=2 # 열기/닫기 형식이 이전거랑 같으면 곱하기
     elif string[i] == "[":
         stack.append(string[i])
         if i==0 or string[i-1] == "(" or string[i-1] == "[" \
         or string[i-1] == ")" or string[i-1] == "]":
-            tmp*=3
+            tmp*=3 # 열기/닫기 형식이 이전거랑 같으면 곱하기
     
     # 닫는 괄호일 때
     elif string[i] == ")":
@@ -27,17 +33,17 @@ for i in range(len(string)):
             answer = 0
             break
         if string[i-1] == "(":
-            answer += tmp
+            answer += tmp # 열기/닫기 쌍이 안 맞으면 더하기
         stack.pop()
-        tmp //=2
+        tmp //=2 # 열기/닫기 쌍이 맞으면 나누기
     elif string[i] == "]":
         if len(stack)==0 or stack[-1] == "(":
             answer = 0
             break
         if string[i-1] == "[":
-            answer += tmp
+            answer += tmp # 열기/닫기 쌍이 안 맞으면 더하기
         stack.pop()
-        tmp //=3
+        tmp //=3 # 열기/닫기 쌍이 맞으면 나누기
     # print(i, string[i], tmp, answer)
 
 if len(stack) != 0:
